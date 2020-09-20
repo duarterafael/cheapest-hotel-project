ae5cd798a0e08a655cbcdfd30283028139cd9a7c
Rafael <rafaelbatistaduarte@gmail.com>
2020-09-20 10:24:49 -0300

    Input by main

---
+++ cheapest-hotel-project/src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.text.ParseException;
+import java.util.Scanner;
+
+import control.HotelController;
+import model.Hotel;
+import view.Input;
+import view.InputParser;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.print("Digite a entrada: ");
+		Scanner sc = new Scanner(System.in);
+		String strInput = "";
+		try {
+			strInput = sc.nextLine();
+			Input input = InputParser.execute(strInput);
+			Hotel hotel = HotelController.getCheaperHotel(input);
+			System.out.println("O hotel mais barato para a entrada "+strInput+" é "+hotel.getName());
+		} catch (ParseException e) {
+			System.out.println("A entrada: "+strInput+" gerou a exceção: "+e.getMessage());
+		} finally {
+			sc.close();
+		}
+		
+		
+	}
+	
+	
+}
